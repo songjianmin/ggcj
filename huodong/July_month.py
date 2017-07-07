49a39893d684719eb3613266668d56e7ada67a56
songjianmin <317804344@qq.com>
2017-07-07 15:42:38 +0800

    v1

---
+++ huodong/July_month.py
@@ -0,0 +1,16 @@
+#!use/bin/env python
+# -*- coding:utf-8 -*-
+
+from selenium import webdriver
+
+
+class July_Active():
+
+    def __init__(self):
+        pass
+
+    def open_browser(self):
+        pass
+
+    def adjust_browser(self):
+        pass
