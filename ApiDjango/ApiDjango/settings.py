3533d75d4b41209a71eebde110bced2e4f4542a0
songjianmin <317804344@qq.com>
2017-07-18 15:53:08 +0800

    设置django打印queryset 执行时对应的sql语句

--- ApiDjango/ApiDjango/settings.py
+++ ApiDjango/ApiDjango/settings.py
@@ -128,3 +128,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGGING = {
+    'version':1,
+    'disable_existing_loggers':False,
+    'handlers':{
+        'console':{
+            'class':'logging.StreamHandler',
+        },
+    },
+    'loggers':{
+        'django.db.backends':{
+            'handlers':['console'],
+            'level':'DEBUG' if DEBUG else 'INFO',
+        },
+    },
+}
