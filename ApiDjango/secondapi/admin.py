2aed9c6d905a6a7a7da889655be9fed2968203f7
songjianmin <317804344@qq.com>
2017-07-19 17:47:36 +0800

    后台增加搜索、过滤

--- ApiDjango/secondapi/admin.py
+++ ApiDjango/secondapi/admin.py
@@ -5,6 +5,8 @@ from .models import Author,Article,Tag,userinfo,Person
 
 class authorlist(admin.ModelAdmin):
     list_display = ['name','qq','addr','email']
+    search_fields = ['name','qq','addr']
+    list_filter = ['name','qq',]
 
 class articlelist(admin.ModelAdmin):
     list_display = ['title','author','content','score',Tag]
@@ -14,6 +16,10 @@ class taglist(admin.ModelAdmin):
 
 class personlist(admin.ModelAdmin):
     list_display = ['my_property',]
+
+
+# class PersonAdmin(admin.ModelAdmin):
+#     list_filter = ('my_property',)
 
 admin.site.register(Author,authorlist)
 admin.site.register(Article,articlelist)
