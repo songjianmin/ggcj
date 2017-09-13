6eb751d017533e8fa7e3b7509a7fe2368b208cbf
songjianmin <317804344@qq.com>
2017-09-13 10:56:24 +0800

    Excel中批量转换时间戳为日期格式（TimevalueToDate.py）

--- huodong/TimevalueToDate.py
+++ huodong/TimevalueToDate.py
@@ -6,7 +6,7 @@ import datetime,time
 
 excel = load_workbook(r'E:\呱呱财经\测试报告\元宝消费原始数据.xlsx')
 
-table = excel.get_sheet_by_name('Sheet2')
+table = excel.get_sheet_by_name('Sheet5')
 
 nrows = table.max_row
 ncols = table.max_column
@@ -17,7 +17,7 @@ print (nrows,ncols)
 # time.localtime(1500262276683)
 
 for i in range(2,nrows+1):
-    timevalue = int(table.cell(row=i,column=3).value/1000)
+    timevalue = int(table.cell(row=i,column=2).value/1000)
     dateArray = time.localtime(timevalue)
     datestr = time.strftime("%Y/%m/%d",dateArray)
     # print (type(timevalue))
